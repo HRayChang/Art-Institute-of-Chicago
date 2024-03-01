1d05ff21a818870169eb87aad5fed7ff807b8c90
RayChang <raychang861205@gmail.com>
2024-03-01 18:04:04 +0800

    start Art Institute of Chicago project

---
+++ Art Institute of Chicago/Art_Institute_of_ChicagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_Institute_of_ChicagoApp.swift
+//  Art Institute of Chicago
+//
+//  Created by Ray Chang on 2024/3/1.
+//
+
+import SwiftUI
+
+@main
+struct Art_Institute_of_ChicagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Art Institute of Chicago/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Art Institute of Chicago
+//
+//  Created by Ray Chang on 2024/3/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
