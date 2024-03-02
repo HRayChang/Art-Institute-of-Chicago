c84cfc805f00802aa1a6f27b6d7b6ed637c457f2
RayChang <raychang861205@gmail.com>
2024-03-02 16:32:03 +0800

    setup ArtworkDetailView view

--- Art Institute of Chicago/Views/ArtworkDetailView.swift
+++ Art Institute of Chicago/Views/ArtworkDetailView.swift
@@ -12,7 +12,7 @@ struct ArtworkDetailView: View {
     
     var body: some View {
         ScrollView {
-            // view
+            artworkImageView
             
             Text(artwork.title)
                 .font(.title)
@@ -20,6 +20,20 @@ struct ArtworkDetailView: View {
         }
         .navigationTitle("Artwork Details")
         .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    @ViewBuilder var artworkImageView: some View {
+        if let imageId = artwork.imageId,
+           let imageURL = URL(string: "https://www.artic.edu/iiif/2/\(imageId)/full/843,/0/default.jpg") {
+            AsyncImage(url: imageURL) { image in
+                image.resizable()
+                .aspectRatio(contentMode: .fit)
+            } placeholder: {
+                ProgressView()
+            }
+        } else {
+            Text("Image is not available")
+        }
     }
 }
 
