1f160dd501d3923fdab035f3d7f096054a9d6c64
RayChang <raychang861205@gmail.com>
2024-03-02 16:47:56 +0800

    setup ContentView

--- Art Institute of Chicago/Views/ContentView.swift
+++ Art Institute of Chicago/Views/ContentView.swift
@@ -8,17 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = ArtworkViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(viewModel.artworks) { artwork in
+                ArtworkView(artwork: artwork)
+            }
+            .navigationTitle("Artwork")
+            .onAppear {
+                viewModel.fetchArtwork()
+            }
         }
-        .padding()
     }
 }
-
+    
 #Preview {
     ContentView()
 }
