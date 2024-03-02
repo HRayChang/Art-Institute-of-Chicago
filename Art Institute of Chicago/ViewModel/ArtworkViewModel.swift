dbaf2e28560e5782be5e5618ad66a51fbbba12e6
RayChang <raychang861205@gmail.com>
2024-03-02 15:40:14 +0800

    setup fetchArtwork func

--- Art Institute of Chicago/ViewModel/ArtworkViewModel.swift
+++ Art Institute of Chicago/ViewModel/ArtworkViewModel.swift
@@ -13,5 +13,25 @@ class ArtworkViewModel: ObservableObject {
     private var cancellables = Set<AnyCancellable>()
     private let jsonDecoder = JSONDecoder()
     
-    
+    func fetchArtwork() {
+        guard let url = URL(string: "https://api.artic.edu/api/v1/artworks") else { return }
+        
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: ArtworkDataResponse.self, decoder: jsonDecoder)
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("Error: \(error)")
+                case .finished:
+                    print("It finished!")
+                }
+            } receiveValue: { [weak self] response in
+                self?.artworks = response.data
+            }
+            .store(in: &cancellables)
+    }
 }
