bb1c89a68f7d012dfcbb1d7ccae9a258898ffb2b
RayChang <raychang861205@gmail.com>
2024-03-02 16:12:06 +0800

    create ArtworkDetailView

---
+++ Art Institute of Chicago/Views/ArtworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ArtworkDetailView.swift
+//  Art Institute of Chicago
+//
+//  Created by Ray Chang on 2024/3/2.
+//
+
+import SwiftUI
+
+struct ArtworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtworkDetailView()
+}
