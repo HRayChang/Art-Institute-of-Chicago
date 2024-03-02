5d60a131ea0df7f1412ca17d92761c27db75b7ca
RayChang <raychang861205@gmail.com>
2024-03-02 15:01:14 +0800

    setup ArtworkDataResponse and previewArtwork

--- Art Institute of Chicago/Model/Artwork.swift
+++ Art Institute of Chicago/Model/Artwork.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct ArtworkDataResponse: Decodable {
+    let data: [Artwork]
+}
+
+struct Artwork: Decodable, Identifiable {
+    let id: Int
+    let title: String
+    let imageId: String?
+}
+
+let previewArtwork = Artwork(id: 16487, title: "The Bay of Marseille, Seen from L\'Estaque", imageId: "d4ca6321-8656-3d3f-a362-2ee297b2b813")
